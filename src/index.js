40dd96c36f435b7d575be421e5e1bf0c2bb70c52
OlesRezvaniuk <orezvaniuk@gmail.com>
2022-09-20 18:12:57 +0300

    r
    r

--- src/index.js
+++ src/index.js
@@ -1,43 +1,19 @@
 import axios from 'axios';
 
 axios
-  .get('https://pixabay.com/api/', {
-    params: {
-      q: '',
-      orientation: 'horizontal',
-      image_type: 'photo',
-      orientation: 'horizontal',
-      safesearch: 'true',
-    },
-    headers: {
-      key: '30029348-12068a2fdca19007a6804d89e',
-    },
-  })
+  .get('https://pixabay.com/api/?key=30029348-12068a2fdca19007a6804d89e&q')
   .then(res => {
     console.log(res.data);
   })
   .catch(error => {
     console.log(error.toJSON());
   });
-
-// axios.post('https://jsonplaceholder.typicode.com/posts', {
-//   userId: 1,
-//   title: 'My title omg',
-//   body: 'My omg body',
-// });
-
-// axios({
-//   method: 'POST',
-//   url: 'https://jsonplaceholder.typicode.com/posts',
-//   data: {
-//     userId: 1,
-//     title: 'My title omg',
-//     body: 'My omg body',
-//   },
-// })
-//   .then(resp => {
-//     console.log(resp.data);
-//   })
-//   .catch(err => {
-//     console.log(err);
-//   });
+//
+axios
+  .get('https://pixabay.com/api/?key=30029348-12068a2fdca19007a6804d89e&q')
+  .then(res => {
+    console.log(res.data);
+  })
+  .catch(error => {
+    console.log(error.toJSON());
+  });
