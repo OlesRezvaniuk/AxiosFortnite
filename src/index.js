9f629a0aad48b0bb693ccd5c40a78c09e33374d1
OlesRezvaniuk <orezvaniuk@gmail.com>
2022-09-21 13:07:58 +0300

    r
    r

--- src/index.js
+++ src/index.js
@@ -1,1 +1,25 @@
-console.log('hi');
+import axios from 'axios';
+// const axios = require('axios');
+const axios = require('axios').default;
+
+const refs = {
+  btn: document.querySelector('.btn'),
+  img: document.querySelector('.img'),
+  url: 'http://aws.random.cat//meow',
+};
+
+refs.btn.addEventListener('click', () => {
+  fetchHandler();
+});
+
+async function fetchHandler() {
+  try {
+    const resp = await axios.get(refs.url);
+    // const data = await resp.json();
+    console.log(resp);
+    refs.img.src = resp.data.file;
+  } catch (err) {
+    console.log(err);
+  }
+}
+fetchHandler();
